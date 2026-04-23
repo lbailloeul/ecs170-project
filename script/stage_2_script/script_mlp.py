24a632eb96b5f0311367e22f7a182a91247dec1b
Leo Bailloeul <leobailloeul@Leos-MacBook-Pro.local>
2026-04-22 17:46:52 -0700

    added performance metric plots in pipeline

--- script/stage_2_script/script_mlp.py
+++ script/stage_2_script/script_mlp.py
@@ -1,9 +1,9 @@
-from code.stage_1_code.Dataset_Loader import Dataset_Loader
-from code.stage_1_code.Method_MLP import Method_MLP
-from code.stage_1_code.Result_Saver import Result_Saver
-from code.stage_1_code.Setting_KFold_CV import Setting_KFold_CV
-from code.stage_1_code.Setting_Train_Test_Split import Setting_Train_Test_Split
-from code.stage_1_code.Evaluate_Accuracy import Evaluate_Accuracy
+from code.stage_2_code.Dataset_Loader import Dataset_Loader
+from code.stage_2_code.Method_MLP import Method_MLP
+from code.stage_2_code.Result_Saver import Result_Saver
+from code.stage_2_code.Setting_KFold_CV import Setting_KFold_CV
+from code.stage_2_code.Setting_Train_Test_File import Setting_Train_Test_File
+from code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
 import numpy as np
 import torch
 
@@ -15,19 +15,20 @@ if 1:
     #------------------------------------------------------
 
     # ---- objection initialization setction ---------------
-    data_obj = Dataset_Loader('toy', '')
-    data_obj.dataset_source_folder_path = '../../data/stage_1_data/'
-    data_obj.dataset_source_file_name = 'toy_data_file.txt'
+    data_obj = Dataset_Loader('stage 2', '')
+    data_obj.dataset_source_folder_path = '../../data/stage_2_data/'
+    data_obj.dataset_train_file_name = 'train.csv'
+    data_obj.dataset_test_file_name = 'test.csv'
+
 
     method_obj = Method_MLP('multi-layer perceptron', '')
 
     result_obj = Result_Saver('saver', '')
-    result_obj.result_destination_folder_path = '../../result/stage_1_result/MLP_'
+    result_obj.result_destination_folder_path = '../../result/stage_2_result/MLP_'
     result_obj.result_destination_file_name = 'prediction_result'
 
-    setting_obj = Setting_KFold_CV('k fold cross validation', '')
-    #setting_obj = Setting_Tra
-    # in_Test_Split('train test split', '')
+    # setting_obj = Setting_KFold_CV('k fold cross validation', '')
+    setting_obj = Setting_Train_Test_File('train test split', '')
 
     evaluate_obj = Evaluate_Accuracy('accuracy', '')
     # ------------------------------------------------------
